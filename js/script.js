2cef08f8d9f832f5bf61d37d92fae0657fdac91f
Stefany Cortez <stefanygct@gmail.com>
2024-12-11 00:16:28 -0300

    Se crean archivos script.js y productos.json, se enlazan documentos y se traen productosTortas dinamicamente

---
+++ js/script.js
@@ -0,0 +1,387 @@
+// const tortasCaseras = [
+//   {
+//     id: 1,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Caseras/Casera1.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 2,
+//     nombre: "Torta Tres Leches",
+//     imagen: "/images/Tortas/Tortas Caseras/Casera2.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 3,
+//     nombre: "Marquesa de Chocolate",
+//     imagen: "/images/Tortas/Tortas Caseras/Casera3.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 4,
+//     nombre: "Chocotorta",
+//     imagen: "/images/Tortas/Tortas Caseras/Casera4.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 5,
+//     nombre: "Torta de Piña",
+//     imagen: "/images/Tortas/Tortas Caseras/Casera5.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+// ];
+
+// const tortasCumple = [
+//   {
+//     id: 6,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Cumple/Cumple1.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 7,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Cumple/Cumple2.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 8,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Cumple/Cumple3.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 9,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Cumple/Cumple4.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 10,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Cumple/Cumple5.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+// ];
+// const tortasFondant = [
+//   {
+//     id: 11,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Fondant/Fondant1.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 12,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Fondant/Fondant2.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 13,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Fondant/Fondant3.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 14,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Fondant/Fondant4.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 15,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Fondant/Fondant5.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+// ];
+
+// const tortasGolosina = [
+//   {
+//     id: 16,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Golosinas/Golosinas1.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 17,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Golosinas/Golosinas2.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 18,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Golosinas/Golosinas3.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 19,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Golosinas/Golosinas4.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+//   {
+//     id: 20,
+//     nombre: "Torta Selva Negra",
+//     imagen: "/images/Tortas/Tortas Golosinas/Golosinas5.png",
+//     descripcion:
+//       "Torta con Bizcochuelo húmedo de Chocolate con doble relleno de crema con frutos rojos, cubierta con baño de Ganache de Chocolate y Frutos Rojos",
+//     precio: 7000,
+//     cantidad: 10,
+//   },
+// ];
+
+// const tortaCaseras = tortasCaseras;
+// const tortaCumple = tortasCumple;
+// const tortaFondant = tortasFondant;
+// const tortaGolosinas = tortasGolosina;
+// const producto = productos.slice(0, 6);
+
+fetch("../js/productos.json")
+  .then((response) => response.json())
+      .then((productos) => {
+        const artCaseras = document.querySelector(".articleTortasCaseras");
+
+productos["tortasCaseras"].forEach((prod) => {
+  const html = `
+
+    <div class="galleryContainer">
+      <div class="boton-abrir">
+          <label for="btn-modal">
+              <div class="gallery">
+                <img src="${prod.imagen}" alt="${prod.nombre}"/>
+                <div class="desc"><p>${prod.nombre}</p></div>
+              </div>
+          </label>
+      </div>
+
+      <!-- Ventana modal  -->
+
+<input type="checkbox" id="btn-modal">
+<div class="container-modal modal">
+        <div class="content-modal">
+            <div>
+            <h3 class="description">${prod.nombre}<h3/>
+            </div>
+            <img class="img-modal" src="${prod.imagen}" alt="${prod.nombre}" />
+            <p class="details">${prod.descripcion}</p>
+            <p class="price">$ ${prod.precio}</p>
+            <button class="btn">Agregar al Carrito</button>
+        </div>
+        <label for="btn-modal" class="cerrar-modal"></label>
+        <div class="boton-cerrar">
+                <label for="btn-modal">X</label>
+          </div>
+      </div>
+`;
+
+  artCaseras.innerHTML += html;
+});
+
+const artCumple = document.querySelector(".articleTortasCumple");
+
+productos["tortasCumple"].forEach((prod) => {
+  const html = `
+
+    <div class="galleryContainer">
+      <div class="boton-abrir">
+          <label for="btn-modal">
+              <div class="gallery">
+                <img src="${prod.imagen}" alt="${prod.nombre}"/>
+                 <div class="desc"><p>${prod.nombre}</p></div>
+              </div>
+          </label>
+      </div>
+
+      <!-- Ventana modal  -->
+
+<input type="checkbox" id="btn-modal">
+<div class="container-modal modal">
+        <div class="content-modal">
+            <div>
+            <h3 class="description">${prod.nombre}<h3/>
+            </div>
+            <img class="img-modal" src="${prod.imagen}" alt="${prod.nombre}" />
+            <p class="details">${prod.descripcion}</p>
+            <p class="price">$ ${prod.precio}</p>
+            <button class="btn">Agregar al Carrito</button>
+        </div>
+        <label for="btn-modal" class="cerrar-modal"></label>
+        <div class="boton-cerrar">
+                <label for="btn-modal">X</label>
+          </div>
+      </div>
+`;
+
+  artCumple.innerHTML += html;
+});
+
+const artFondant = document.querySelector(".articleTortasFondant");
+
+productos["tortasFondant"].forEach((prod) => {
+  const html = `
+
+    <div class="galleryContainer">
+      <div class="boton-abrir">
+          <label for="btn-modal">
+              <div class="gallery">
+                <img src="${prod.imagen}" alt="${prod.nombre}"/>
+                <div class="desc"><p>${prod.nombre}</p></div>
+              </div>
+          </label>
+      </div>
+
+      <!-- Ventana modal  -->
+
+<input type="checkbox" id="btn-modal">
+<div class="container-modal modal">
+        <div class="content-modal">
+            <div>
+            <h3 class="description">${prod.nombre}<h3/>
+            </div>
+            <img class="img-modal" src="${prod.imagen}" alt="${prod.nombre}" />
+            <p class="details">${prod.descripcion}</p>
+            <p class="price">$ ${prod.precio}</p>
+            <button class="btn">Agregar al Carrito</button>
+        </div>
+        <label for="btn-modal" class="cerrar-modal"></label>
+        <div class="boton-cerrar">
+                <label for="btn-modal">X</label>
+          </div>
+      </div>
+`;
+
+  artFondant.innerHTML += html;
+});
+
+
+const artGolosinas = document.querySelector(".articleTortasGolosinas");
+
+productos["tortasGolosinas"].forEach((prod) => {
+  const html = `
+
+    <div class="galleryContainer">
+      <div class="boton-abrir">
+          <label for="btn-modal">
+              <div class="gallery">
+                <img src="${prod.imagen}" alt="${prod.nombre}"/>
+                <div class="desc"><p>${prod.nombre}</p></div>
+              </div>
+          </label>
+      </div>
+
+      <!-- Ventana modal  -->
+
+<input type="checkbox" id="btn-modal">
+<div class="container-modal modal">
+        <div class="content-modal">
+            <div>
+            <h3 class="description">${prod.nombre}<h3/>
+            </div>
+            <img class="img-modal" src="${prod.imagen}" alt="${prod.nombre}" />
+            <p class="details">${prod.descripcion}</p>
+            <p class="price">$ ${prod.precio}</p>
+            <button class="btn">Agregar al Carrito</button>
+        </div>
+        <label for="btn-modal" class="cerrar-modal"></label>
+        <div class="boton-cerrar">
+                <label for="btn-modal">X</label>
+          </div>
+      </div>
+`;
+
+  artGolosinas.innerHTML += html;
+});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+ 
+ 
+
+
+
+// Se agrega subtítulos a cada article
+
+const tituloCaseras = document.querySelector(".articleTortasCaseras");
+const subtituloCaseras = document.createElement("h2");
+subtituloCaseras.classList.add("subtitle");
+subtituloCaseras.textContent = "TORTAS CASERAS";
+document.getElementById("productTortas").insertBefore(subtituloCaseras, tituloCaseras);
+
+const tituloCumple = document.querySelector(".articleTortasCumple");
+const subtituloCumple = document.createElement("h2");
+subtituloCumple.classList.add("subtitle");
+subtituloCumple.textContent = "TORTAS DE CUMPLEAÑOS";
+document.getElementById("productTortas").insertBefore(subtituloCumple, tituloCumple);
+
+const tituloFondant = document.querySelector(".articleTortasFondant");
+const subtituloFondant = document.createElement("h2");
+subtituloFondant.classList.add("subtitle");
+subtituloFondant.textContent = "TORTAS DE FONDANT";
+document.getElementById("productTortas").insertBefore(subtituloFondant, tituloFondant);
+
+const tituloGolosinas = document.querySelector(".articleTortasGolosinas");
+const subtituloGolosinas = document.createElement("h2");
+subtituloGolosinas.classList.add("subtitle");
+subtituloGolosinas.textContent = "TORTAS CON GOLOSINAS";
+document.getElementById("productTortas").insertBefore(subtituloGolosinas, tituloGolosinas);
+
