843bf90ddf835a1d942d2b8074f22e59f5413670
Stefany Cortez <stefanygct@gmail.com>
2024-12-11 00:27:47 -0300

    Se modifican imágenes en productos.json

--- js/script.js
+++ js/script.js
@@ -197,11 +197,11 @@
 
 fetch("../js/productos.json")
   .then((response) => response.json())
-      .then((productos) => {
-        const artCaseras = document.querySelector(".articleTortasCaseras");
+  .then((productos) => {
+    const artCaseras = document.querySelector(".articleTortasCaseras");
 
-productos["tortasCaseras"].forEach((prod) => {
-  const html = `
+    productos["tortasCaseras"].forEach((prod) => {
+      const html = `
 
     <div class="galleryContainer">
       <div class="boton-abrir">
@@ -233,13 +233,13 @@ productos["tortasCaseras"].forEach((prod) => {
       </div>
 `;
 
-  artCaseras.innerHTML += html;
-});
+      artCaseras.innerHTML += html;
+    });
 
-const artCumple = document.querySelector(".articleTortasCumple");
+    const artCumple = document.querySelector(".articleTortasCumple");
 
-productos["tortasCumple"].forEach((prod) => {
-  const html = `
+    productos["tortasCumple"].forEach((prod) => {
+      const html = `
 
     <div class="galleryContainer">
       <div class="boton-abrir">
@@ -271,52 +271,13 @@ productos["tortasCumple"].forEach((prod) => {
       </div>
 `;
 
-  artCumple.innerHTML += html;
-});
+      artCumple.innerHTML += html;
+    });
 
-const artFondant = document.querySelector(".articleTortasFondant");
+    const artFondant = document.querySelector(".articleTortasFondant");
 
-productos["tortasFondant"].forEach((prod) => {
-  const html = `
-
-    <div class="galleryContainer">
-      <div class="boton-abrir">
-          <label for="btn-modal">
-              <div class="gallery">
-                <img src="${prod.imagen}" alt="${prod.nombre}"/>
-                <div class="desc"><p>${prod.nombre}</p></div>
-              </div>
-          </label>
-      </div>
-
-      <!-- Ventana modal  -->
-
-<input type="checkbox" id="btn-modal">
-<div class="container-modal modal">
-        <div class="content-modal">
-            <div>
-            <h3 class="description">${prod.nombre}<h3/>
-            </div>
-            <img class="img-modal" src="${prod.imagen}" alt="${prod.nombre}" />
-            <p class="details">${prod.descripcion}</p>
-            <p class="price">$ ${prod.precio}</p>
-            <button class="btn">Agregar al Carrito</button>
-        </div>
-        <label for="btn-modal" class="cerrar-modal"></label>
-        <div class="boton-cerrar">
-                <label for="btn-modal">X</label>
-          </div>
-      </div>
-`;
-
-  artFondant.innerHTML += html;
-});
-
-
-const artGolosinas = document.querySelector(".articleTortasGolosinas");
-
-productos["tortasGolosinas"].forEach((prod) => {
-  const html = `
+    productos["tortasFondant"].forEach((prod) => {
+      const html = `
 
     <div class="galleryContainer">
       <div class="boton-abrir">
@@ -348,16 +309,50 @@ productos["tortasGolosinas"].forEach((prod) => {
       </div>
 `;
 
-  artGolosinas.innerHTML += html;
-});
-      })
-      .catch((error) => {
-        console.log(error);
-      });
- 
- 
+      artFondant.innerHTML += html;
+    });
 
+    const artGolosinas = document.querySelector(".articleTortasGolosinas");
 
+    productos["tortasGolosinas"].forEach((prod) => {
+      const html = `
+
+    <div class="galleryContainer">
+      <div class="boton-abrir">
+          <label for="btn-modal">
+              <div class="gallery">
+                <img src="${prod.imagen}" alt="${prod.nombre}"/>
+                <div class="desc"><p>${prod.nombre}</p></div>
+              </div>
+          </label>
+      </div>
+
+      <!-- Ventana modal  -->
+
+<input type="checkbox" id="btn-modal">
+<div class="container-modal modal">
+        <div class="content-modal">
+            <div>
+            <h3 class="description">${prod.nombre}<h3/>
+            </div>
+            <img class="img-modal" src="${prod.imagen}" alt="${prod.nombre}" />
+            <p class="details">${prod.descripcion}</p>
+            <p class="price">$ ${prod.precio}</p>
+            <button class="btn">Agregar al Carrito</button>
+        </div>
+        <label for="btn-modal" class="cerrar-modal"></label>
+        <div class="boton-cerrar">
+                <label for="btn-modal">X</label>
+          </div>
+      </div>
+`;
+
+      artGolosinas.innerHTML += html;
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // Se agrega subtítulos a cada article
 
@@ -365,23 +360,30 @@ const tituloCaseras = document.querySelector(".articleTortasCaseras");
 const subtituloCaseras = document.createElement("h2");
 subtituloCaseras.classList.add("subtitle");
 subtituloCaseras.textContent = "TORTAS CASERAS";
-document.getElementById("productTortas").insertBefore(subtituloCaseras, tituloCaseras);
+document
+  .getElementById("productTortas")
+  .insertBefore(subtituloCaseras, tituloCaseras);
 
 const tituloCumple = document.querySelector(".articleTortasCumple");
 const subtituloCumple = document.createElement("h2");
 subtituloCumple.classList.add("subtitle");
 subtituloCumple.textContent = "TORTAS DE CUMPLEAÑOS";
-document.getElementById("productTortas").insertBefore(subtituloCumple, tituloCumple);
+document
+  .getElementById("productTortas")
+  .insertBefore(subtituloCumple, tituloCumple);
 
 const tituloFondant = document.querySelector(".articleTortasFondant");
 const subtituloFondant = document.createElement("h2");
 subtituloFondant.classList.add("subtitle");
 subtituloFondant.textContent = "TORTAS DE FONDANT";
-document.getElementById("productTortas").insertBefore(subtituloFondant, tituloFondant);
+document
+  .getElementById("productTortas")
+  .insertBefore(subtituloFondant, tituloFondant);
 
 const tituloGolosinas = document.querySelector(".articleTortasGolosinas");
 const subtituloGolosinas = document.createElement("h2");
 subtituloGolosinas.classList.add("subtitle");
 subtituloGolosinas.textContent = "TORTAS CON GOLOSINAS";
-document.getElementById("productTortas").insertBefore(subtituloGolosinas, tituloGolosinas);
-
+document
+  .getElementById("productTortas")
+  .insertBefore(subtituloGolosinas, tituloGolosinas);
